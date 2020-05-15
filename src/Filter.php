58701ae53660cfb69c9ce14aa858b527d592e220
pmatsuda <pat.matsuda@comcast.net>
2020-05-15 11:03:33 -0500

    Updated  Filter class - break getFiltered and applied SRP

--- src/Filter.php
+++ src/Filter.php
@@ -38,39 +38,52 @@ class Filter implements FilterInterface
     {
         if ($this->_data && empty($this->_filteredData)) {
 
-            /**
-             * Only keep alphanumeric, ',', '-', space.  '"' in names were replaced with non-space like the rest
-             */
-            $temp_name = preg_replace("/\'{2}/", " ", $this->_data['account_name']);
-            $this->_filteredData['account_name']  = preg_replace("/[^[:alnum:],\- ]+/", "", $temp_name);
-            //$this->_filteredData['account_name']  = preg_replace("/[^[:alnum:],\- ]+/", "", $this->_data['account_name']);
-
-            /**
-             * Check if length of account number is less than 10, if less than 10, pad with 0s at the beginning of the number
-             */
-            $length = strlen($this->_data['account_number']);
-            if ($length < 10)
-            {
-                $this->_filteredData['account_number'] = str_pad($this->_data['account_number'], 10, "0", STR_PAD_LEFT);
-            }
-            else
-            {
-                $this->_filteredData['account_number'] = $this->_data['account_number'];
-            }
-
-            /**
-             * Round the transaction fee to 2 digits for the cent value
-             */
-            $this->_filteredData['transaction_fee']  = number_format($this->_data['transaction_fee'], 2);
-
-            /**
-             * Remove all non-numeric characters, then only keep the last 10 - area code and the number.
-             * Format sanitized phone number as xxx-xxx-xxxx
-             */
-            $temp  = substr(preg_replace("/[^[[:digit:]]+/", "", $this->_data['phone_number']), -10);
-            $this->_filteredData['phone_number']  = substr($temp, 0, 3) . "-" . substr($temp, 3, 3) . "-" . substr($temp, 6);
+            $this->_filteredData['account_number'] = $this->filterAccountNumber($this->_data['account_number']);
+            $this->_filteredData['account_name']= $this->filterAccountName($this->_data['account_name']);
+            $this->_filteredData['transaction_fee']  = $this->filterTransactionFee($this->_data['transaction_fee']);
+            $this->_filteredData['phone_number']  = $this->filterPhoneNumber($this->_data['phone_number']);
         }
         return $this->_filteredData;
+    }
+   
+    public function filterAccountNumber($accountNumber)
+    {        
+        /**
+         * Check if length of account number is less than 10, if less than 10, pad with 0s at the beginning of the number
+         */
+        $length = strlen($accountNumber);
+        if ($length < 10)
+        {
+            return str_pad($accountNumber, 10, "0", STR_PAD_LEFT);
+        }
+            return $accountNumber;
+    }
+
+    public function filterAccountName($accountName)
+    {
+        /**
+         * Only keep alphanumeric, ',', '-', space.  '"' in names were replaced with non-space like the rest
+         */
+        $temp_name = preg_replace("/\'{2}/", " ", $accountName);
+        return preg_replace("/[^[:alnum:],\- ]+/", "", $temp_name);
+    }
+
+    public function filterTransactionFee($transactionFee)
+    {
+        /**
+         * Round the transaction fee to 2 digits for the cent value
+         */
+        return number_format(floatval($transactionFee), 2);
+    }
+
+    public function filterPhoneNumber($phoneNumber)
+    {
+        /**
+         * Remove all non-numeric characters, then only keep the last 10 - area code and the number.
+         * Format sanitized phone number as xxx-xxx-xxxx
+         */
+        $temp  = substr(preg_replace("/[^[[:digit:]]+/", "", $phoneNumber), -10);
+        return substr($temp, 0, 3) . "-" . substr($temp, 3, 3) . "-" . substr($temp, 6);
     }
 }
 ?>
